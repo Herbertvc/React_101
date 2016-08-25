209fa31cb6462db0f5f1e02346c3d2dc883e8478
Herbert V <herbert.videla@tektonlabs.com>
2016-08-24 20:42:05 -0500

    Firebase config

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,31 @@
 import {browserHistory} from 'react-router';
+import Firebase from 'firebase';
 
 export const REQUEST_GIFS = 'REQUEST_GIFS';
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+const ref = new Firebase('https://react-giphy-tutorial.firebaseio.com');
 
 export function requestGifs(term = null) {
 	const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
-
-	let data = fetch(url)
-		.then((response) => response.json())
-		.then((data) => data.data);
-
-	return {
-		type: REQUEST_GIFS,
-		payload: data,
+	return function(dispatch) {
+		let data = fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				dispatch({
+					type: REQUEST_GIFS,
+					payload: data.data
+				});
+			})
 	}
+	// return {
+	// 	type: REQUEST_GIFS,
+	// 	payload: data,
+	// }
 }
 
 export function openModal(gif) {
@@ -32,6 +41,21 @@ export function closeModal() {
 	}
 }
 
+export function signUpUser(credentials) {
+	return function(dispatch) {
+		ref.createUser({
+			email: credentials.email,
+			password: credentials.password,
+		}, function (error, userData) {
+			if (error) {
+				dispatch(authError(error));
+			} else {
+				dispatch(signInUser(credentials));
+			}
+		});
+	}
+}
+
 export function signInUser() {
 	browserHistory.push('/favorites');
 
@@ -44,5 +68,12 @@ export function signOutUser() {
 	browserHistory.push('/');
 	return {
 		type: SIGN_OUT_USER,
+	}
+}
+
+export function authError(error) {
+	return {
+		type: AUTH_ERROR,
+		payload: error,
 	}
 }
